5238a849612b1770433929d16c0d95879661b8bc
Joshua Baert <jbghoring@gmail.com>
2017-06-20 13:09:47 -0500

    Codewars Challenge

---
+++ 7-kyu/movesInSquared.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Joshua Baert on 6/20/2017.
+ */
+
+// https://www.codewars.com/kata/moves-in-squared-strings-i
+
+function vertMirror(s) {
+	return s.split('\n').map(e => e.split('').reverse().join('')).join('\n')
+}
+function horMirror(s) {
+	return s.split('\n').reverse().join('\n')
+}
+function oper(fct, s) {
+	return fct(s)
+}
+
+console.log(oper(horMirror, 'dcba\nhgfe\nlkji\nponm'))
