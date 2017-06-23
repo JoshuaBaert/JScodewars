9b617ff4bba200bb3d01ef252e5dcfdf6721e811
Joshua Baert <jbghoring@gmail.com>
2017-06-23 18:44:40 -0500

    Codewars Challenge

---
+++ 7-kyu/helloWorldAgain.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Joshua Baert on 6/23/2017.
+ */
+
+//https://www.codewars.com/kata/oh-come-on-hello-world-once-again/
+
+f=_=>'\x48\x65\x6c\x6c\x6f\x2c\x20\x77\x6f\x72\x6c\x64\x21'
+
+console.log(f());
