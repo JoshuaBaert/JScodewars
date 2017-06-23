174ec88901e7f0824709ae758408523ddfa80124
Joshua Baert <jbghoring@gmail.com>
2017-06-23 16:57:28 -0500

    Codewars Challenge

--- 6-kyu/movesInSquared2.js
+++ 6-kyu/movesInSquared2.js
@@ -1,3 +1,20 @@
 /**
  * Created by Joshua Baert on 6/22/2017.
  */
+
+function rot(s) {
+	return s.split('\n').reverse().map(e => e.split('').reverse().join('')).join('\n')
+}
+function selfieAndRot(s) {
+	let dots = ''
+	s.split('\n')[0].split('').map(e=> dots += '.')
+	return s.split('\n').map(e=>e+=dots)
+		.concat(rot(s).split('\n').map(e=>dots+e)).join('\n')
+}
+function oper(fct, s) {
+	return fct(s);
+}
+
+console.log(oper(selfieAndRot,'xZBV\njsbS\nJcpN\nfVnP'));
+console.log('Should Be')
+console.log('xZBV....\njsbS....\nJcpN....\nfVnP....\n....PnVf\n....NpcJ\n....Sbsj\n....VBZx')
