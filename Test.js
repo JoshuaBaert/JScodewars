0bec35c4bbf85f0ec3bfc635e29efaf08915e56a
Joshua Baert <jbghoring@gmail.com>
2017-07-25 21:00:08 -0500

    Codewars Challenge

--- Test.js
+++ Test.js
@@ -2,8 +2,8 @@
  * Created by Joshua Baert on 5/5/2017.
  */
 
-//https://www.codewars.com/kata/oh-come-on-hello-world-once-again/
+// https://www.codewars.com/kata/counting-duplicates
 
-f=_=>'\x48\x65\x6c\x6c\x6f\x2c\x20\x77\x6f\x72\x6c\x64\x21'
+function duplicateCount(text){
 
-console.log(f());
+}
